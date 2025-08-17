10e6bf9792f551f471798340092bc02739de50a5
MogesBekele <mogesbekele32@gmail.com>
2025-08-17 12:43:11 +0300

    create the dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,50 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+
+export default function Home() {
+  return (
+    <main className=" flex flex-col gap-6 justify-center h-full text-center max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+      </div>
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium text-left p-4">
+              <span className="font-semibold">12/11/2024</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span className="font-semibold">Acme Inc</span>
+            </TableCell>
+            <TableCell className="text-left p-4">LcYlF@example.com</TableCell>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full ">Open</Badge>
+            </TableCell>
+            <TableCell className="text-right">
+              <span className="font-semibold">Birr 250.00</span>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <div className=" flex justify-center items-center h-screen text-3xl font-bold">
-    <h1>this is home page for invoice app</h1>
-  </div>
+    <main className=" flex flex-col gap-6 justify-center h-screen text-center max-w-5xl mx-auto">
+      <h1 className="text-5xl fond-bold">Invoicing-app</h1>
+      <p>
+        <Button asChild>
+          <Link href="/dashboard">Sign In</Link>
+        </Button>
+      </p>
+    </main>
   );
 }
