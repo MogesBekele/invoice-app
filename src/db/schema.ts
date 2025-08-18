14f701eba9b435804bf0ef2ec9b26b6df7241fac
MogesBekele <mogesbekele32@gmail.com>
2025-08-18 07:36:08 +0300

    postgres database config

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, serial, text, integer, timestamp, pgEnum } from "drizzle-orm/pg-core";
+export const statusEnum = pgEnum("status", ["open", "paid", "void", "uncollectible"]);
+
+export const Invoices = pgTable("invoices", {
+  id: serial("id").primaryKey().notNull(),
+  createTs: timestamp("create_ts").defaultNow().notNull(),
+  value: integer("value").notNull(),
+  description: text("description").notNull(),
+  status: statusEnum("status").notNull(),
+});
