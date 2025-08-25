b6dbaa3a78a65698a397efad23edd68b04108e85
MogesBekele <mogesbekele32@gmail.com>
2025-08-25 12:55:09 +0300

    adjust the margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen max-sm:mt-[-100px]">
+    <main className="flex flex-col items-center justify-center min-h-screen max-sm:mt-[-150px]">
       <Button
         asChild
         className="px-6 py-3 bg-cyan-600 text-white rounded-lg shadow-lg hover:bg-cyan-700 transition"
