f01f0a1d81a9258ea892a568ae4f19887b34a0df
MogesBekele <mogesbekele32@gmail.com>
2025-08-25 12:59:12 +0300

    resp/footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,11 @@ import Container from "./Container";
 
 const Footer = () => {
   return (
-    <footer className="pb-6 pt-6">
-      <Container className="flex justify-center flex-col items-center text-gray-900">
+    <footer className="pb-6 pt-6 px-4">
+      <Container className="flex flex-col items-center text-center gap-2 text-gray-900">
         <p className="text-sm">
-          Created by Moges Bekele with Next.js, Postgres, Xata, Clerk and Stripe for payments.
+          Created by Moges Bekele with Next.js, Postgres, Xata, Clerk and Stripe
+          for payments.
         </p>
         <p className="text-sm">
           Invoicing-app &copy; {new Date().getFullYear()}
