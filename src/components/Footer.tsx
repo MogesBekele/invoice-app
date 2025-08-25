bb8c8a362ae2b81dc9c7baf7a7ff42e322a87120
MogesBekele <mogesbekele32@gmail.com>
2025-08-25 13:07:39 +0300

    xs/footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="pb-6 pt-6 px-4">
       <Container className="flex flex-col items-center text-center gap-2 text-gray-900">
-        <p className="text-xm">
+        <p className="text-xs">
           Created by Moges Bekele with Next.js, Postgres, Xata, Clerk and Stripe
           for payments.
         </p>
