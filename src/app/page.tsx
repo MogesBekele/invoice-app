b10e2ad37e67e3d3ed8abd149ba6dbfbde6fd5ad
MogesBekele <mogesbekele32@gmail.com>
2025-08-26 11:49:27 +0300

    fix main page issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <Link
         href="/dashboard"
         className="px-6 py-3 bg-gray-800 text-white rounded-lg shadow-lg hover:bg-gray-900 transition text-lg font-semibold"
       >
         Go to Dashboard
       </Link>
-    </main>
+    </div>
   );
 }
