62c42b9840d8613de74191b28cc56cb4c888c9f1
MogesBekele <mogesbekele32@gmail.com>
2025-08-26 12:31:25 +0300

    fix pad/issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex items-center justify-center min-h-screen bg-white ">
       <Link
         href="/dashboard"
-        className="px-6 py-3 bg-gray-800 text-white rounded-lg shadow-lg hover:bg-gray-900 transition text-lg font-semibold"
+        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-900 transition text-lg font-semibold"
       >
         Go to Dashboard
       </Link>
--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+//app/sign-in/[[..rest]]/page.tsx
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
