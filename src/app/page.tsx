b6c799b4f1834be82e43673f3cc0dbac6e3bb092
MogesBekele <mogesbekele32@gmail.com>
2025-08-26 14:06:14 +0300

    bg/dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-white ">
       <Link
         href="/dashboard"
-        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-900 transition text-lg font-semibold"
+        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition text-lg font-semibold"
       >
         Go to Dashboard
       </Link>
