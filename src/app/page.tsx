597645a5a1c10d155a1731c7bc4fd90d370b463f
MogesBekele <mogesbekele32@gmail.com>
2025-08-27 11:17:04 +0300

    payment integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white ">
+    <div className="flex items-center justify-center h-screen bg-white ">
       <Link
         href="/dashboard"
-        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition text-lg font-semibold"
+        className="px-6 py-3 bg-gray-50  text-gray-800 rounded-lg shadow-lg hover:bg-gray-100 transition text-lg font-semibold"
       >
         Go to Dashboard
       </Link>
--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-//app/sign-in/[[..rest]]/page.tsx
-
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
