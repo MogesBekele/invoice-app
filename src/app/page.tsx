66d51e9382fe497c5c635d87ac2121e6b7cb53b4
MogesBekele <mogesbekele32@gmail.com>
2025-08-27 11:26:52 +0300

    pad/in production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
-      <div className="-translate-y-10 max-sm:-translate-y-40"> {/* Moves content slightly above center */}
+      <div className="-translate-y-10 max-sm:-translate-y-20"> {/* Moves content slightly above center */}
         <Link
           href="/dashboard"
           className="px-6 py-3 bg-gray-50 text-gray-800 rounded-lg shadow-lg hover:bg-gray-100 transition text-lg font-semibold"
